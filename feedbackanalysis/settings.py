0587af4c54a6c5ef7a313338cec47fe4949995a4
Abhishek Kumar <abhi.ckp1002@gmail.com>
2019-08-10 12:10:58 +0530

    file upload

--- feedbackanalysis/settings.py
+++ feedbackanalysis/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r+gtnz8)rn86^^!z9snbc1qa(zd(e1)xo^_n=7!*+c9zkmhv(p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
